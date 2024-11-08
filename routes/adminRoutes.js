465a03aec0e1fd4194e33de91090344e04f6d17a
fThAbhishek-Pandey <abhishek830564@email.com>
2024-11-09 01:10:30 +0530

    bug fixing

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express'
 import Login from '../Controlers/admin/login.js';
 import authAdmin from '../middleware/admin/authAdmin.js';
-import addEmploy from '../Controlers/Admin/addEmploy.js';
-import reciptHistory from '../Controlers/Admin/getRecieptHistory.js';
-import sendMail from '../Controlers/Admin/sendmail.js';
+import addEmploy from '../Controlers/admin/addEmploy.js';
+import reciptHistory from '../Controlers/admin/getRecieptHistory.js';
+import sendMail from '../Controlers/admin/sendmail.js';
 const AdminRouter = express.Router();
 AdminRouter.post('/login', Login);
 AdminRouter.post('/add-doctor', [authAdmin],addEmploy);
