cb9c821309cb8277bfa6b4dc8e5efb09d7567152
fThAbhishek-Pandey <abhishek830564@email.com>
2024-11-09 11:47:34 +0530

    Adding exetution path

--- Controlers/admin/generatepdf.js
+++ Controlers/admin/generatepdf.js
@@ -4,6 +4,7 @@ async function generatePDF(htmlTemplate) {
   const browser = await puppeteer.launch({
     headless: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    executablePath:process.env.PUPPETEER_EXECUTABLE_PATH
   });
 
   const page = await browser.newPage();
